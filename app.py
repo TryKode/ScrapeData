026687cef7540fbc2b7b6ccc95551653557b71b8
TryKode <97229651+TryKode@users.noreply.github.com>
2022-01-11 03:12:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
 @app.get('/scrape')
 def scrape_data(database_scrape:database_scrape):
     productURL = database_scrape.product_url
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
     productPage = requests.get(productURL, headers=headers)
     productSoup = bs(productPage.content,'html.parser')
     
